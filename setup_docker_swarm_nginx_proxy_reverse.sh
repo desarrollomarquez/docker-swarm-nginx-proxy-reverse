79d15b5bb4946221b711bf3f85648ce92d55c806
Cloud-DevOps <dfmarque@unal.edu.co>
2023-08-08 23:36:44 -0500

    Ajuste setup sh

--- setup_docker_swarm_nginx_proxy_reverse.sh
+++ setup_docker_swarm_nginx_proxy_reverse.sh
@@ -24,20 +24,18 @@ sudo docker build --tag proxy-reverse/nginx ./nginx/
 
 sudo docker build --tag backend/app-node ./backend/app-node/
 
-#sudo docker service create --name backend-app-node-swarm -p 80:5000 --replicas 2 backend/app-node
-
 # map app-node service to overlay network
 sudo docker service create --name backend-app-node-swarm --replicas 2 --network swarm-net backend/app-node
 
 #Frontend: 
 
-sudo docker build --tag frontend/app-node ./frontend/app-node/
-#sudo docker service create --name frontend-app-node-swarm -p 81:3000 --replicas 2 frontend/app-node
+#sudo docker build --tag frontend/app-node ./frontend/app-node/
+
 # map app-node service to overlay network
-sudo docker service create --name frontend-app-node-swarm --replicas 2 --network swarm-net frontend/app-node
+#sudo docker service create --name frontend-app-node-swarm --replicas 2 --network swarm-net frontend/app-node
 
 # map nginx service to overlay network and expose ports 80 and 443
-sudo docker service create --name nginx-swarm --replicas 1 -p 80:80 -p 443:443 --network swarm-net proxy-reverse/nginx
+#sudo docker service create --name nginx-swarm --replicas 1 -p 80:80 -p 443:443 --network swarm-net proxy-reverse/nginx
 
 sudo docker service ls
 
